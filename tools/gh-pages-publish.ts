93b0e6020383d3e2e500ee8b3df29990def64349
Horus Lugo <horusgoul@gmail.com>
2019-03-05 02:39:11 +0100

    ci(gh-pages-publis.ts): Prevents publishing GitHub pages on pull requests

--- tools/gh-pages-publish.ts
+++ tools/gh-pages-publish.ts
@@ -2,6 +2,11 @@ const { cd, exec, echo, touch } = require("shelljs");
 const { readFileSync } = require("fs");
 const url = require("url");
 
+if (process.env.TRAVIS_PULL_REQUEST !== "false") {
+  echo("This run was triggered by a pull request and therefore the docs won't be published.");
+  process.exit(0);
+}
+
 let repoUrl;
 let pkg = JSON.parse(readFileSync("package.json") as any);
 if (typeof pkg.repository === "object") {
@@ -22,8 +27,8 @@ cd("docs");
 touch(".nojekyll");
 exec("git init");
 exec("git add .");
-exec("git config user.name \"Horus Lugo\"");
-exec("git config user.email \"horusgoul@gmail.com\"");
-exec("git commit -m \"docs(docs): update gh-pages\"");
+exec(`git config user.name "Horus Lugo"`);
+exec(`git config user.email "horusgoul@gmail.com"`);
+exec(`git commit -m "docs(docs): update gh-pages"`);
 exec(`git push --force --quiet "https://${ghToken}@${repository}" master:gh-pages`);
 echo("Docs deployed!!");
