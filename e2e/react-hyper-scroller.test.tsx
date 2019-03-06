4ab065bd9b7e7725f55569293c49ed0f1d4503ce
Horus Lugo <horusgoul@gmail.com>
2019-03-06 23:08:24 +0100

    test(e2e): Adds new e2e test that scrolls through the VirtualScroller list

--- e2e/react-hyper-scroller.test.tsx
+++ e2e/react-hyper-scroller.test.tsx
@@ -1,4 +1,7 @@
-import { launch, LaunchOptions } from "puppeteer";
+import { launch, LaunchOptions, Browser } from "puppeteer";
+import { ITEMS_TO_GENERATE } from "./test-app/src/utils";
+
+jest.setTimeout(32000);
 
 const CONFIG: LaunchOptions =
   process.env.NODE_ENV === "debug"
@@ -9,8 +12,8 @@ const CONFIG: LaunchOptions =
       }
     : {};
 
-describe("on page load", async () => {
-  let browser;
+describe("e2e tests", async () => {
+  let browser: Browser;
 
   beforeAll(async () => {
     browser = await launch(CONFIG);
@@ -20,24 +23,70 @@ describe("on page load", async () => {
     await browser.close();
   });
 
-  test(
-    "h1 loads correctly",
-    async () => {
-      const page = (await browser.pages())[0];
+  test("VirtualScroller loads correctly", async () => {
+    const page = await browser.newPage();
 
-      page.emulate({
-        viewport: {
-          width: 500,
-          height: 720,
-        },
-        userAgent: "",
+    page.emulate({
+      viewport: {
+        width: 500,
+        height: 720,
+      },
+      userAgent: "",
+    });
+
+    await page.goto("http://localhost:3001");
+
+    const html = await page.$eval("#root", e => e.innerHTML);
+    expect(html).toContain(`Item 0`);
+  });
+
+  test("scrolls through the list", async () => {
+    const page = await browser.newPage();
+
+    page.emulate({
+      viewport: {
+        width: 500,
+        height: 720,
+      },
+      userAgent: "",
+    });
+
+    await page.goto("http://localhost:3001");
+
+    await page.$eval("#root", e => e.innerHTML);
+
+    for (let i = 0; i < ITEMS_TO_GENERATE; i++) {
+      const endReached = await page.evaluate(() => {
+        if (window.scrollY + window.innerHeight >= document.body.offsetHeight) {
+          return true;
+        }
+
+        return false;
       });
 
-      await page.goto("http://localhost:3001");
+      if (endReached) {
+        break;
+      }
 
-      const html = await page.$eval("#root", e => e.innerHTML);
-      expect(html).toContain(`Item 0`);
-    },
-    16000,
-  );
+      await page.evaluate(`
+        (async () => {
+          if (window.scrollY + window.innerHeight >= document.body.offsetHeight) {
+            return;
+          }
+
+          const timeout = new Promise((resolve, reject) => {
+            setTimeout(() => {
+              window.scrollBy(0, window.innerHeight);
+              resolve();
+            }, 50);
+          });
+
+          await timeout;
+        })();
+      `);
+    }
+
+    const html = await page.$eval("#root", e => e.innerHTML);
+    expect(html).toContain(`Item ${ITEMS_TO_GENERATE - 1}`);
+  });
 });
